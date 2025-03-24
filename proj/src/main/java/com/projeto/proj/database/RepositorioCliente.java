4ffb344c6516456805ac54f4cf2a9608209985c3
enzowarikoda <119605031+enzowarikoda@users.noreply.github.com>
2025-03-24 20:17:23 -0300

    Delete proj directory

--- proj/src/main/java/com/projeto/proj/database/RepositorioCliente.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.proj.database;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.projeto.proj.entidade.Cliente;
-
-public interface RepositorioCliente extends JpaRepository<Cliente, Long> {
-    
-}
